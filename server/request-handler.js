35ec62262d306be0d0fe4285e0dc49a7fd264a9d
Hack Reactor Students <jakobharclerode@gmail.com>
2014-06-23 20:10:34 -0700

    passing 6 tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,45 +1,40 @@
-/* You should implement your request handler function in this file.
- * And hey! This is already getting passed to http.createServer()
- * in basic-server.js. But it won't work as is.
- * You'll have to figure out a way to export this function from
- * this file and include it in basic-server.js so that it actually works.
- * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
-
-var handleRequest = function(request, response) {
-  /* the 'request' argument comes from nodes http module. It includes info about the
-  request - such as what URL the browser is requesting. */
-
-  /* Documentation for both request and response can be found at
-   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
-
-  console.log("Serving request type " + request.method + " for url " + request.url);
-
+var RequestHandler = function(){};
+RequestHandler.prototype.handler = function(request, response) {
   var statusCode = 200;
+  var obj = {};
+  obj.results = [];
 
-  /* Without this line, this server wouldn't work. See the note
-   * below about CORS. */
-  var headers = defaultCorsHeaders;
+  var url = require('url').parse(request.url);
+  if(url.pathname === "/classes/room1"){
+    if(request.method === 'GET'){
+      var body = JSON.stringify(obj);
+      response.writeHead(200,{"Content-Type": "application/json"});
+      response.end(body);
 
-  headers['Content-Type'] = "text/plain";
+    }else if(request.method === 'POST'){
+      var body = JSON.stringify(obj);
+      response.writeHead(201,{"Content-Type": "application/json"});
+      response.end(body);
+    }
+  }else{// default handler
 
-  /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
-
-  /* Make sure to always call response.end() - Node will not send
-   * anything back to the client until you do. The string you pass to
-   * response.end() will be the body of the response - i.e. what shows
-   * up in the browser.*/
-  response.end("Hello, World!");
+    var defaultCorsHeaders = {
+      "access-control-allow-origin": "*",
+      "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "access-control-allow-headers": "content-type, accept",
+      "access-control-max-age": 10}
+  // Seconds.
+    var headers = defaultCorsHeaders;
+    headers['Content-Type'] = "text/plain";
+    response.writeHead(200, headers);
+    request.on('data',function(msg){
+      obj.results.push(msg)
+      console.log(obj.results)  ;
+    });
+    response.end('hello PLANET EARTH');
+  }
 };
+module.exports = new RequestHandler();
 
-/* These headers will allow Cross-Origin Resource Sharing (CORS).
- * This CRUCIAL code allows this server to talk to websites that
- * are on different domains. (Your chat client is running from a url
- * like file://your/chat/client/index.html, which is considered a
- * different domain.) */
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
-};
+
+
--- server/spec/ServerSpec.js
+++ server/spec/ServerSpec.js
@@ -14,9 +14,10 @@ function waitForThen(test, cb) {
 describe('Node Server Request Listener Function', function() {
   it('Should answer GET requests for /classes/room with a 200 status code', function() {
     // This is a fake server request. Normally, the server would provide this,
-    // but we want to test our function's behavior totally independent of the server code
     var req = new stubs.request('/classes/room1', 'GET');
+   // but we want to test our function's behavior totally independent of the server code
     var res = new stubs.response();
+
 
     handler.handler(req, res);
 
