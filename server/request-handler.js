f5628dc2296ae0072e3f615b648aa9be96eb8d77
Hack Reactor Students <jakobharclerode@gmail.com>
2014-06-24 12:01:57 -0700

    finished base requirements

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,40 +1,37 @@
-var RequestHandler = function(){};
-RequestHandler.prototype.handler = function(request, response) {
-  var statusCode = 200;
-  var obj = {};
-  obj.results = [];
-
-  var url = require('url').parse(request.url);
-  if(url.pathname === "/classes/room1"){
-    if(request.method === 'GET'){
-      var body = JSON.stringify(obj);
-      response.writeHead(200,{"Content-Type": "application/json"});
-      response.end(body);
-
-    }else if(request.method === 'POST'){
-      var body = JSON.stringify(obj);
-      response.writeHead(201,{"Content-Type": "application/json"});
-      response.end(body);
-    }
-  }else{// default handler
-
-    var defaultCorsHeaders = {
-      "access-control-allow-origin": "*",
-      "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "access-control-allow-headers": "content-type, accept",
-      "access-control-max-age": 10}
-  // Seconds.
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10
+};
+var url = require('url');
+var obj = {};
+obj.results = [];
+var handleRequest = function(request, response) {
     var headers = defaultCorsHeaders;
-    headers['Content-Type'] = "text/plain";
-    response.writeHead(200, headers);
-    request.on('data',function(msg){
-      obj.results.push(msg)
-      console.log(obj.results)  ;
-    });
-    response.end('hello PLANET EARTH');
+    headers['Content-Type'] = "application/json";
+    if(request.url.indexOf('class') !== -1){
+      if(request.method === 'GET'){
+        response.writeHead(200,headers);
+        response.end(JSON.stringify(obj));
+      }
+      if(request.method === 'POST'){
+        var data = '';
+        request.on('data',function(chunk){
+          data += chunk;
+          console.log(data)
+        });
+        request.on('end',function(){
+          response.writeHead(201, headers);
+          obj.results.push(JSON.parse(data));
+          response.end(JSON.stringify(obj));
+        });
+      }
+  }else{
+    response.writeHead(404, headers);
+    response.end(JSON.stringify(obj));
   }
 };
-module.exports = new RequestHandler();
 
 
-
+module.exports.handler = handleRequest;
